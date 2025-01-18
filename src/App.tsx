ae0a2c3951c6a8afcf9da3d9e5fff442f909fda1
Sun-ny11 <spetrow79@gmail.com>
2025-01-18 13:30:23 +0300

    fix: styles and add Liked svg

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Card />
       <Card />
       <Card />
-      <Card />
+      <Card src="https://cdn2.thecatapi.com/images/125.jpg" alt="cat" />
       <Card />
       <Card />
       <Card />
--- src/common/components/card/Card.tsx
+++ src/common/components/card/Card.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef } from "react";
 import s from "./Card.module.css";
 import clsx from "clsx";
+import HeartLike from "../../../assets/heart";
 
 export const Card = ({
   className,
@@ -9,6 +10,9 @@ export const Card = ({
   return (
     <div className={s.main}>
       <img {...rest} className={clsx(s.img, className)} />
+      <button className={s.likeBtn}>
+        <HeartLike className={s.heartLike} />
+      </button>
     </div>
   );
 };
