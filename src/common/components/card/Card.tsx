7fe9e841721bb42204ad5af154a63e26d1c80ed0
Sun-ny11 <spetrow79@gmail.com>
2025-01-21 01:46:27 +0300

    routingAndApi: added routing and project logic (#3)
    * chore: add react-router 7
    
    * chore: add zustend
    
    * chore: add react-toastify
    
    * feat: add likeButton component
    
    * refactor: card component
    
    * feat: add cards component
    
    * feat: add useElementOnScreen hook
    
    * feat: add responseErrorHandler util
    
    * refactor: layout component and add toast
    
    * feat: add Loader component
    
    * feat: add react router dom
    
    * feat: allCats feature
    
    * feat: favoriteCats feature
    
    * chore: add ghPages

--- src/common/components/card/Card.tsx
+++ src/common/components/card/Card.tsx
@@ -1,18 +1,23 @@
 import { ComponentPropsWithoutRef } from "react";
 import s from "./Card.module.css";
 import clsx from "clsx";
-import HeartLike from "../../../assets/heart";
 
-export const Card = ({
-  className,
-  ...rest
-}: ComponentPropsWithoutRef<"img">) => {
+import { LikeButton } from "../likeButton/LikeButton";
+
+type Props = {
+  callBack?: () => void;
+  isLiked: boolean;
+} & ComponentPropsWithoutRef<"img">;
+
+export const Card = ({ callBack, className, isLiked, ...rest }: Props) => {
   return (
     <div className={s.main}>
       <img {...rest} className={clsx(s.img, className)} />
-      <button className={s.likeBtn}>
-        <HeartLike className={s.heartLike} />
-      </button>
+      <LikeButton
+        isLiked={isLiked}
+        className={clsx(s.likeBtn, isLiked && s.liked)}
+        onClick={callBack}
+      />
     </div>
   );
 };
---
+++ src/features/allCards/ui/AllCards.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useCats } from "../model/catStore";
+import { Loader } from "../../../common/components/loader/Loader";
+import { Cards } from "../../../common/components/cards/Cards";
+import { useElementOnScreen } from "../../../common/hooks/useElementOnScreen";
+
+export const AllCards = () => {
+  const cats = useCats((state) => state.cats);
+  const loading = useCats((state) => state.loading);
+  const toggleLikeCats = useCats((state) => state.toggleLikeCats);
+  const fetchCats = useCats((state) => state.fetchCats);
+  const { ref, visible } = useElementOnScreen<HTMLDivElement>();
+  const [triggerLoading, setTriggerLoading] = useState(false);
+
+  useEffect(() => {
+    const triggerRerender = () => {
+      if (!ref.current) {
+        return;
+      }
+      const windowHeight = document.documentElement.clientHeight;
+      const containerRect = ref.current.getBoundingClientRect();
+      const spaceBelow = windowHeight - containerRect.bottom > 40;
+
+      if (spaceBelow) {
+        setTriggerLoading((prev) => !prev);
+      }
+    };
+
+    if (visible) {
+      fetchCats().then(() => triggerRerender());
+    }
+  }, [visible, fetchCats, triggerLoading, ref]);
+
+  return (
+    <>
+      <Cards handleLikeClick={toggleLikeCats} cats={cats} ref={ref} />
+      {loading && <Loader />}
+    </>
+  );
+};
