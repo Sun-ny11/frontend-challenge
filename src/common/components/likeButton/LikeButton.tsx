1ea506231e1f9d2a34f4d6880fce14a09fd9dd7a
Sun-ny11 <spetrow79@gmail.com>
2025-01-21 15:05:52 +0300

    refactor: hover on heart

--- src/common/components/likeButton/LikeButton.tsx
+++ src/common/components/likeButton/LikeButton.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, useState } from "react";
 import HeartLike from "../../../assets/heart";
 import HeartLiked from "../../../assets/liked";
 import clsx from "clsx";
@@ -7,9 +7,16 @@ type Props = {
   isLiked: boolean;
 } & ComponentPropsWithoutRef<"button">;
 export const LikeButton = ({ isLiked, className, ...rest }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <button className={clsx(s.likeBtn, className)} {...rest}>
-      {isLiked ? <HeartLiked /> : <HeartLike />}
+    <button
+      className={clsx(s.likeBtn, className)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      {...rest}
+    >
+      {isHovered || isLiked ? <HeartLiked /> : <HeartLike />}
     </button>
   );
 };
