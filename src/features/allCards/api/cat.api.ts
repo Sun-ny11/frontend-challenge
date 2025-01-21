b39e2470a5d9a89230fb6fbdb5595a49a21664f2
Sun-ny11 <spetrow79@gmail.com>
2025-01-21 15:26:09 +0300

    refactor: api metods

--- src/features/allCards/api/cat.api.ts
+++ src/features/allCards/api/cat.api.ts
@@ -2,13 +2,13 @@ import { AxiosInstance, AxiosResponse } from "axios"
 import { Endpoints } from "./cat.endpoints"
 import { instance } from "../../../common/api/instance"
 import { CatImage } from "../model/types"
-import { DislikeType, FavouriteCatResponse } from "./cat.api.types"
+import { DislikeType, FavouriteCatResponse, FetchCats } from "./cat.api.types"
 import { Nullable } from "../../../common/types"
 
 class CatApi {
     constructor(private instance: AxiosInstance) {}
 
-    public async fetchCat(page:number = 0, userId?:Nullable<string>,signal?:AbortSignal, limit:number = 15, order:string = "DESC"): Promise<CatImage[]> {
+    async fetchCat({page = 0, userId, signal, limit = 15, order = "DESC"}:FetchCats): Promise<CatImage[]> {
       const res:AxiosResponse<CatImage[]> = await this.instance.get(Endpoints.fetchCats,{
         params: {
           page,
@@ -21,14 +21,14 @@ class CatApi {
 
       return res.data
     }
-    public async likeCat(catId:string, userId:Nullable<string>): Promise<FavouriteCatResponse> {
+    async likeCat(catId:string, userId:Nullable<string>): Promise<FavouriteCatResponse> {
       const res:AxiosResponse<FavouriteCatResponse> = await this.instance.post(Endpoints.favouriteCats,{       
           image_id: catId,
           sub_id: userId,
       })
       return res.data
     }
-    public async dislikeCat(favouriteId:number): Promise<DislikeType> {
+    async dislikeCat(favouriteId:number): Promise<DislikeType> {
       const res:AxiosResponse<DislikeType> = await this.instance.delete(Endpoints.favouriteCats + `/${favouriteId}`)
       return res.data
 
--- src/features/allCards/api/cat.api.types.ts
+++ src/features/allCards/api/cat.api.types.ts
@@ -1,6 +1,18 @@
+import { Nullable } from "../../../common/types";
 
 export type FavouriteCatResponse = {
     id: number;
     message:string
   };
+
 export type DislikeType = Omit<FavouriteCatResponse, 'id'>
+
+export type FetchCats = {
+  page:number 
+  userId?:Nullable<string> 
+  signal?:AbortSignal 
+  limit:number
+  order:string 
+
+
+}
